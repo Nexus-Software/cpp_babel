c96ea6d68d6ae3f30baad9c0dccad70fbe19c3f8
Enguerrand Allamel <enguerrand.allamel@epitech.eu>
2017-09-20 17:06:00 +0200

    ServerInit(feat): Ajout main.cpp + CMakeFile for compiling

---
+++ Server/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+** main.cpp for cpp_babel in /home/enguerrand/delivery/cpp_babel/main.cpp
+**
+** Made by Enguerrand Allamel
+** Login   <enguerrand.allamel@epitech.eu>
+**
+** Started on  mer. sept. 20 16:32:04 2017 Enguerrand Allamel
+** Last update mer. sept. 20 16:32:04 2017 Enguerrand Allamel
+*/
+
+#include <iostream>
+
+int	main(int , char**)
+{
+  std::cout << "Server start" << std::endl;
+  return (EXIT_SUCCESS);
+}
