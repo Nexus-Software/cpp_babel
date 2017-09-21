0a0997256a38e412dcf2b79dc98fdf583b7bf359
JSX <jean-sebastien.herbaux@epitech.eu>
2017-09-22 00:15:51 +0200

    architectureManagers(feat): Adding working core manager with loading of sub managers

--- Client/src/managers/Core/class/BabelClientManager/BabelClientManager.cpp
+++ Client/src/managers/Core/class/BabelClientManager/BabelClientManager.cpp
@@ -8,3 +8,33 @@ babel::BabelClientManager::BabelClientManager() {
 babel::BabelClientManager::~BabelClientManager() {
 	std::cout << "BabelClientManager destructed" << std::endl;
 }
+
+const babel::GlobalMediaManager & babel::BabelClientManager::getMedia(void) const
+{
+	return this->_media;
+}
+
+const babel::UIManager & babel::BabelClientManager::getUI(void) const
+{
+	return this->_ui;
+}
+
+const babel::NetworkManager & babel::BabelClientManager::getNetwork(void) const
+{
+	return this->_network;
+}
+
+const babel::ContactManager & babel::BabelClientManager::getContact(void) const
+{
+	return this->_contact;
+}
+
+const babel::Call & babel::BabelClientManager::getCurrentCall(void) const
+{
+	return this->_currentCall;
+}
+
+const babel::Status babel::BabelClientManager::run(void)
+{
+	return babel::Status(0, "Everything is right!");
+}
