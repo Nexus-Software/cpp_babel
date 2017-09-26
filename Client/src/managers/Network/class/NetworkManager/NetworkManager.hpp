2fb789b5b89628e1c5e1e6005e4e40bea2c024ef
JSX <jean-sebastien.herbaux@epitech.eu>
2017-09-26 13:50:25 +0200

    networkManagerClient(feat): Adding netorkManager implementation

--- Client/src/managers/Network/class/NetworkManager/NetworkManager.hpp
+++ Client/src/managers/Network/class/NetworkManager/NetworkManager.hpp
@@ -1,15 +1,29 @@
 #ifndef BABEL_NETWORKMANAGER_HPP_
 # define BABEL_NETWORKMANAGER_HPP_
 
+#include <memory>
+#include "Status.hpp"
+#include "QNetworkTcpClient.hpp"
+
+namespace babel {
+	class BabelClientManager;
+} // babel
+
 namespace babel {
 	class NetworkManager
 	{
 		public:
-			NetworkManager();
+			NetworkManager(babel::BabelClientManager&);
 			~NetworkManager();
 
 		private:
+			babel::BabelClientManager&			_root;
+			std::shared_ptr<INetworkTcpClient>	_networkTcp;
 
+		public:
+			babel::BabelClientManager& getRoot(void) const;
+			const std::shared_ptr<INetworkTcpClient> getNetworkTcp(void) const;
+			std::shared_ptr<INetworkTcpClient> getNetworkTcp(void);
 	};
 }
 
