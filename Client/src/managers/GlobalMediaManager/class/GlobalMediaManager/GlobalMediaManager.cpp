1408dc5767669d0fa5542cdfcea9b61da2b55520
JSX <jean-sebastien.herbaux@epitech.eu>
2017-09-27 14:26:10 +0200

    mediaManagerClient(feat): Adding GlobalMediaManager

--- Client/src/managers/GlobalMediaManager/class/GlobalMediaManager/GlobalMediaManager.cpp
+++ Client/src/managers/GlobalMediaManager/class/GlobalMediaManager/GlobalMediaManager.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include "GlobalMediaManager.hpp"
+#include "BabelClientManager.hpp"
 
-babel::GlobalMediaManager::GlobalMediaManager() {
+babel::GlobalMediaManager::GlobalMediaManager(babel::BabelClientManager& ancestor)
+:
+	_root(ancestor),
+	_audioManager(*this)
+{
 	std::cout << "GlobalMediaManager created" << std::endl;
 }
 
 babel::GlobalMediaManager::~GlobalMediaManager() {
 	std::cout << "GlobalMediaManager destructed" << std::endl;
+}
+
+babel::BabelClientManager& babel::GlobalMediaManager::getRoot(void) const
+{
+	return this->_root;
+}
+
+// SOUND
+bool 	babel::GlobalMediaManager::playSound()
+{
+	return this->_audioManager.playSound();
+}
+
+bool 	babel::GlobalMediaManager::getSound()
+{
+	return this->_audioManager.getSound();
+}
+
+bool 	babel::GlobalMediaManager::encodeSound()
+{
+	return this->_audioManager.encodeSound();
+}
+
+bool 	babel::GlobalMediaManager::decodeSound()
+{
+	return this->_audioManager.decodeSound();
 }
--- Client/src/managers/GlobalMediaManager/class/GlobalMediaManager/GlobalMediaManager.hpp
+++ Client/src/managers/GlobalMediaManager/class/GlobalMediaManager/GlobalMediaManager.hpp
@@ -1,14 +1,33 @@
 #ifndef BABEL_GLOBALMEDIAMANAGER_HPP_
 # define BABEL_GLOBALMEDIAMANAGER_HPP_
 
+#include "AudioManager.hpp"
+
+namespace babel {
+	class BabelClientManager;
+} // babel
+
 namespace babel {
 	class GlobalMediaManager
 	{
 		public:
-			GlobalMediaManager();
+			GlobalMediaManager(babel::BabelClientManager&);
 			~GlobalMediaManager();
 
 		private:
+			babel::BabelClientManager&	_root;
+			babel::AudioManager 		_audioManager;
+
+		public:
+			babel::BabelClientManager& getRoot(void) const;
+
+		// Sound methods
+		public:
+			bool 	playSound(void);
+			bool 	getSound(void);
+			bool 	encodeSound(void);
+			bool 	decodeSound(void);
+
 
 	};
 }
