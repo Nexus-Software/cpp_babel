7a1f9b71428bae2a4d3acceeebf2c35265420cd8
JSX <jean-sebastien.herbaux@epitech.eu>
2017-09-28 01:56:15 +0200

    mediaManagerClient(test): Adding test in main for soud record / replay

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -2,7 +2,11 @@
 
 int main(int argc, char **argv)
 {
-    babel::BabelClientManager babel(argc, argv);
+	babel::BabelClientManager babel(argc, argv);
 
-    return babel.run();
+	// babel.getMedia().setAudioStreamState(true);
+	// while (1)
+	// 	babel.getMedia().playSound(babel.getMedia().recordSound(), RECORD_SIZE);
+	// babel.getMedia().setAudioStreamState(false);
+	return babel.run();
 }
