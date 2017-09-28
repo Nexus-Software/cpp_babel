9e86788dcbbc9942cfcb1ada62ea716653523a97
Enguerrand <engue0774@gmail.com>
2017-09-28 23:36:13 +0200

    ServerContactList(fix): Fix code return success on login / sign up

--- Server/src/Cmd/Cmd/CmdLogIn.cpp
+++ Server/src/Cmd/Cmd/CmdLogIn.cpp
@@ -35,7 +35,7 @@ bool babel::CmdLogIn::run(size_t tunnelId,  NetworkData & data)
       this->_server.getNetworkManager().get()->getTunnelInfoByTunnelId(tunnelId).setIsAuth(true);
       this->_server.getAccountManager().getAccountByLogin(login).setIsOnline(true);
 
-      this->_server.getNetworkManager().get()->write(tunnelId, NetworkData(42, 0, {}));
+      this->_server.getNetworkManager().get()->write(tunnelId, NetworkData(44, 0, {}));
 
       this->_server.getAccountManager().sendContactList(tunnelId,
 							this->_server.getNetworkManager().get()->getTunnelInfoByTunnelId(tunnelId).login);
