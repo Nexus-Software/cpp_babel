505437eababf8aadecd281a1c43a805612dd0cb8
Enguerrand <engue0774@gmail.com>
2017-09-29 22:11:32 +0200

    ServerContactList(fix): Chanche check duplicate into add / remove

--- Server/src/Account/Account.cpp
+++ Server/src/Account/Account.cpp
@@ -54,7 +54,7 @@ void babel::Account::setIsOnline(bool isOnline)
 
 bool babel::Account::addContact(const std::string &login)
 {
-  if (login == this->getLogin() && std::find(this->_contactList.begin(), this->_contactList.end(), login) != this->_contactList.end())
+  if (login == this->getLogin() || std::find(this->_contactList.begin(), this->_contactList.end(), login) != this->_contactList.end())
     return false;
   this->_contactList.push_back(login);
   return true;
@@ -62,7 +62,7 @@ bool babel::Account::addContact(const std::string &login)
 
 bool babel::Account::removeContact(const std::string &login)
 {
-  if (std::find(this->_contactList.begin(), this->_contactList.end(), login) == this->_contactList.end())
+  if (login == this->getLogin() || std::find(this->_contactList.begin(), this->_contactList.end(), login) == this->_contactList.end())
     return false;
   auto it = std::find(this->_contactList.begin(), this->_contactList.end(), login);
   this->_contactList.erase(it);
