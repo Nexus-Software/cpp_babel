e1aa78fa16b790c2d3e4f73e6a11f1e3dfa54a52
Enguerrand <engue0774@gmail.com>
2017-11-03 00:03:13 +0100

    SetupTravis(fix): Change name opus lib file name

--- Client/cmake_modules/FindOpus.cmake
+++ Client/cmake_modules/FindOpus.cmake
@@ -1,2 +1,2 @@
 find_path(Opus_INCLUDE_DIR NAMES opus.h PATHS ${PROJECT_SOURCE_DIR}/include)
-find_library(Opus_LIBRARY NAMES opus PATHS ${PROJECT_SOURCE_DIR}/lib)
+find_library(Opus_LIBRARY NAMES opus.lib PATHS ${PROJECT_SOURCE_DIR}/lib)
