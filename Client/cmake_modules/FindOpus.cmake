8536b1261850f5af8c9222779a376e12a4b9d491
Enguerrand <engue0774@gmail.com>
2017-11-05 19:15:03 +0100

    SetupTravis(fix): Add install opus

--- Client/cmake_modules/FindOpus.cmake
+++ Client/cmake_modules/FindOpus.cmake
@@ -1,3 +1,2 @@
 find_path(Opus_INCLUDE_DIR NAMES opus.h PATHS ${PROJECT_SOURCE_DIR}/include)
-find_library(Opus_LIBRARY NAMES opus PATHS ${PROJECT_SOURCE_DIR}/lib NO_DEFAULT_PATH)
-message(Opus_LIBRARY)
+find_library(Opus_LIBRARY NAMES opus PATHS ${PROJECT_SOURCE_DIR}/lib)
