f291acf1b82811d10decb4154a1cc766a8fb4d67
Enguerrand <engue0774@gmail.com>
2017-11-05 17:14:40 +0100

    SetupTravis(fix): Change check is opus not found (in target link)

--- Client/cmake_modules/FindOpus.cmake
+++ Client/cmake_modules/FindOpus.cmake
@@ -1,5 +1,2 @@
 find_path(Opus_INCLUDE_DIR NAMES opus.h PATHS ${PROJECT_SOURCE_DIR}/include)
 find_library(Opus_LIBRARY NAMES opus PATHS ${PROJECT_SOURCE_DIR}/lib)
-if (NOT Opus_LIBRARY)
-    set(Opus_LIBRARY ${PROJECT_SOURCE_DIR}/lib/opus)
-endif()
