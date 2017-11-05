1baf3d9f9ca6e6f232d9d584f81cfef84c4e8375
Enguerrand <engue0774@gmail.com>
2017-11-05 17:30:53 +0100

    SetupTravis(fix): Set Opus lib is not found

--- Client/cmake_modules/FindOpus.cmake
+++ Client/cmake_modules/FindOpus.cmake
@@ -1,2 +1,5 @@
 find_path(Opus_INCLUDE_DIR NAMES opus.h PATHS ${PROJECT_SOURCE_DIR}/include)
 find_library(Opus_LIBRARY NAMES opus PATHS ${PROJECT_SOURCE_DIR}/lib)
+if (NOT Opus_LIBRARY)
+    set(Opus_LIBRARY ${PROJECT_SOURCE_DIR}/lib/opus)
+endif()
