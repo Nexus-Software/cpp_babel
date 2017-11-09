0c1b94ca07b29b4248930eb3381b6acf795a1e64
Enguerrand <engue0774@gmail.com>
2017-11-09 11:03:00 +0100

    SetupAppVeyor(feat): Add dir to Server cmake

--- build_system/appveyor.helper.py
+++ build_system/appveyor.helper.py
@@ -8,7 +8,7 @@ print "hello from appveyor.helper.py"
 
 config = os.environ['CONFIG']
 path = os.environ['PATH']
-cmake_command = ['cmake', '-DFMT_PEDANTIC=ON', '-DCMAKE_BUILD_TYPE=' + config]
+cmake_command = ['cmake /Server', '-DFMT_PEDANTIC=ON', '-DCMAKE_BUILD_TYPE=' + config]
 
 os.environ['PATH'] = r'C:\Program Files (x86)\MSBuild\14.0\Bin;' + path
 build_command = ['msbuild', '/m:4', '/p:Config=' + config, 'foo.sln']
